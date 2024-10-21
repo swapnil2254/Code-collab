5d732bc9298074138e82b0069bc8121621727e9a
swapnil2254 <79191667+swapnil2254@users.noreply.github.com>
2024-10-21 10:42:59 +0000

    code collab code setup

---
+++ client/src/components/chats/ChatList.tsx
@@ -0,0 +1,76 @@
+import { useAppContext } from "@/context/AppContext"
+import { useChatRoom } from "@/context/ChatContext"
+import { SyntheticEvent, useEffect, useRef } from "react"
+
+function ChatList() {
+    const {
+        messages,
+        isNewMessage,
+        setIsNewMessage,
+        lastScrollHeight,
+        setLastScrollHeight,
+    } = useChatRoom()
+    const { currentUser } = useAppContext()
+    const messagesContainerRef = useRef<HTMLDivElement | null>(null)
+
+    const handleScroll = (e: SyntheticEvent) => {
+        const container = e.target as HTMLDivElement
+        setLastScrollHeight(container.scrollTop)
+    }
+
+    // Scroll to bottom when messages change
+    useEffect(() => {
+        if (!messagesContainerRef.current) return
+        messagesContainerRef.current.scrollTop =
+            messagesContainerRef.current.scrollHeight
+    }, [messages])
+
+    useEffect(() => {
+        if (isNewMessage) {
+            setIsNewMessage(false)
+        }
+        if (messagesContainerRef.current)
+            messagesContainerRef.current.scrollTop = lastScrollHeight
+    }, [isNewMessage, setIsNewMessage, lastScrollHeight])
+
+    return (
+        <div
+            className="flex-grow overflow-auto rounded-md bg-slate-100  p-2"
+            ref={messagesContainerRef}
+            onScroll={handleScroll}
+        >
+            {/* Chat messages */}
+            {!messages.length && (
+                <div className="flex w-full items-center justify-center ">
+                    {" "}
+                    No messages start chatting...{" "}
+                </div>
+            )}
+            {messages.map((message, index) => {
+                return (
+                    <div
+                        key={index}
+                        className={
+                            "mb-2 w-[80%] self-end break-words rounded-md bg-slate-200 px-3 py-2" +
+                            (message.username === currentUser.username
+                                ? " ml-auto "
+                                : "")
+                        }
+                    >
+                        <div className="flex justify-between">
+                            <span className="text-xs text-primary">
+                                {message.username}
+                            </span>
+                            <span className="text-xs text-dark">
+                                {message.timestamp}
+                            </span>
+                        </div>
+                        <p className="py-1">{message.message}</p>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default ChatList
